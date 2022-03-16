cab9ee7f8ec103c0a59344e430a9b2c2d9dd7286
Zorx <58170434+Zowzow92@users.noreply.github.com>
2022-03-16 17:28:56 +0100

    Update __init__.py

--- Knock Knock/__init__.py
+++ Knock Knock/__init__.py
@@ -1,1 +1,22 @@
+import os
 
+def AffichageMenu():
+    print('Selectionnez un scan :\n'
+          '\t[1] XSS\n'
+          '\t[2] Brute Force\n'
+          '\t[3] Faille d\'Upload\n'
+          '\t[4] Local File Inclusion\n'
+          '\t[5] Quitter\n')
+
+def selectScan(slct):
+    
+    pass    
+    
+slct = -1
+while slct == -1:
+    os.system('cls')
+    AffichageMenu()
+    try:
+        slct = int(input())
+    except ValueError :
+        print("Saisir une valeur entre 1 et 5")
