0e80ea23a6415770c46e364e0e6e5f7802c2e0ea
DeathCreator94 <93936265+DeathCreator94@users.noreply.github.com>
2022-03-17 17:15:03 +0100

    Update __init__.py

--- Knock Knock/__init__.py
+++ Knock Knock/__init__.py
@@ -1,4 +1,8 @@
 import os
+import LFI.py 
+import BruteForce.py
+import FailleXSS.py
+
 
 def AffichageMenu():
     print('Selectionnez un scan :\n'
@@ -8,15 +12,38 @@ def AffichageMenu():
           '\t[4] Local File Inclusion\n'
           '\t[5] Quitter\n')
 
+def AffichageBruteForce():
+    b = BruteForce.BT()
+    b.Attack()
+    b.SearchConsoleAdmin()
+
+def AffichageXSS():
+    url=input("Entrez l'url:") 
+    FailleXSS.scan_xss(url)
+
+def AffichageLFI():
+    LFI.LFI()
+
 def selectScan(slct):
     
     pass    
     
 slct = -1
-while slct == -1:
+fin = 0
+while fin == 0:
     os.system('cls')
     AffichageMenu()
     try:
         slct = int(input())
     except ValueError :
         print("Saisir une valeur entre 1 et 5")
+    if(slct == 1):
+        AffichageXSS()
+    if(slct == 2):
+        AffichageBruteForce()
+    if(slct == 3):
+        AffichageBruteForce()
+    if(slct == 4):
+        AffichageLFI()
+    if(slct == 5):
+        fin = 1
